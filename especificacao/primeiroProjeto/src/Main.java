ae8fdddbb1bde5292771148234e268b4bf9ce72a
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2025-03-13 12:00:00 -0300

    feat: the basics of java

---
+++ especificacao/primeiroProjeto/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Up! Altas Aventuras");
+
+        int ano = 2009;
+        System.out.println("Ano de Lançamento: " + ano);
+
+        boolean includoNoPlano = true;
+
+        double notaDoFilme = 9.9;
+
+        double media = (9.8 + 9.9)/ 2;
+
+        System.out.println(media);
+
+        String sinopse = "Filme de animação";
+        System.out.println(sinopse);
+
+        int estrelas;
+        estrelas = (int) (media/2);
+        System.out.println(estrelas);
+    }
+}
