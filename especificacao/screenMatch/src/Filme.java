aae23030ef6970facf050950ec0d3591b263b8b1
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2025-03-20 12:00:00 -0300

    feat: learning about methods in java

--- especificacao/screenMatch/src/Filme.java
+++ especificacao/screenMatch/src/Filme.java
@@ -2,7 +2,21 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
+    double somaDasAvaliacoes;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    void exibeFichaTécnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double getAvaliacaoDoFilme(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
 }
