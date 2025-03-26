6e78efbdd63da1a7843de59b293b79629e94a3b1
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2025-03-26 12:00:00 -0300

    feat: transformed the film class into an extension class of the title class

--- especificacao/screenMatch/src/leticia/moraes/screenmatch/modelos/Filme.java
+++ especificacao/screenMatch/src/leticia/moraes/screenmatch/modelos/Filme.java
@@ -1,63 +1,13 @@
 package leticia.moraes.screenmatch.modelos;
-public class Filme {
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
+public class Filme extends Titulo{
+    private String diretor;
 
-    public int getTotalDeAvaliacoes(){
-        return totalDeAvaliacoes;
+    public String getDiretor() {
+        return this.diretor;
     }
 
-    public void setNome(String pNome){
-        this.nome = pNome;
+    public void setDiretor(String pDiretor) {
+        this.diretor = pDiretor;
     }
 
-    public String getNome() {
-        return this.nome;
-    }
-
-    public int getAnoDeLancamento() {
-        return this.anoDeLancamento;
-    }
-
-    public void setAnoDeLancamento(int pAnoDeLancamento) {
-        this.anoDeLancamento = pAnoDeLancamento;
-    }
-
-    public boolean isIncluidoNoPlano() {
-        return this.incluidoNoPlano;
-    }
-
-    public void setIncluidoNoPlano(boolean pIncluidoNoPlano) {
-        this.incluidoNoPlano = pIncluidoNoPlano;
-    }
-
-    public double getSomaDasAvaliacoes() {
-        return this.somaDasAvaliacoes;
-    }
-
-    public int getDuracaoEmMinutos() {
-        return this.duracaoEmMinutos;
-    }
-
-    public void setDuracaoEmMinutos(int pDuracaoEmMinutos) {
-        this.duracaoEmMinutos = pDuracaoEmMinutos;
-    }
-
-    public void exibeFichaTécnica(){
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-    }
-
-    public void avaliar(double nota){
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public double getAvaliacaoDoFilme(){
-        return somaDasAvaliacoes / totalDeAvaliacoes;
-    }
 }
